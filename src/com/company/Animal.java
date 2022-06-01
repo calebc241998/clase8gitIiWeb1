d1ea413c2b1dcdf65ba8728691b7aa3dc3acec98
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-06-01 11:20:02 -0500

    animal modificado

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -3,6 +3,15 @@ package com.company;
 public class Animal {
 
     private String raza;
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public String getRaza() {
         return raza;
