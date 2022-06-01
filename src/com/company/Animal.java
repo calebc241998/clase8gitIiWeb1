991863caf152e15aa53be85df69ae4a0aea9bcc2
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-06-01 11:35:48 -0500

    clase animal modificada

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -4,6 +4,15 @@ public class Animal {
 
     private String raza;
     private String nombre;
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
     public String getNombre() {
         return nombre;
