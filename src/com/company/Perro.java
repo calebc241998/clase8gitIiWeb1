29f762292c932bc36f38e2716b35624a744e68c3
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-06-01 11:18:39 -0500

    ajustes en animal y clase perro creada

---
+++ src/com/company/Perro.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Perro extends Animal {
+    private String modeloCorrea;
+
+    public String getModeloCorrea() {
+        return modeloCorrea;
+    }
+
+    public void setModeloCorrea(String modeloCorrea) {
+        this.modeloCorrea = modeloCorrea;
+    }
+}
