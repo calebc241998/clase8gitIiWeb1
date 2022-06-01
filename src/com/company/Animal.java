41f3a7dedc10799961abe1044973df27ba098a3d
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-06-01 11:16:21 -0500

    clase animal y gato creadas

---
+++ src/com/company/Animal.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Animal {
+
+    private String raza;
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+}
